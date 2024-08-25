92cc9b5e5c846535fe1991b99c22439e6a79598e
Yaroslav Avramidi <passenger1177@gmail.com>
2024-08-25 10:05:05 +0300

    fix loader

--- app/components/AnotherLoader.tsx
+++ app/components/AnotherLoader.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
+
 // components/Loader.js
 const AnotherLoader = () => {
   return (
     <div className="loader">
-      <p className='text-2xl md:text-8xl'>Another loader</p>
-      {/* Добавьте стили или анимацию по необходимости */}
+      {/* <p className='text-2xl md:text-8xl'>Another loader</p> */}
+      <Image src='/icon3.png' alt="spiner" width={250} height={250}/>
     </div>
   );
 };
--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import PagesLoader from "../components/PagesLoader";
 
 export default function Home() {
   return (
-    <PagesLoader >
+    // <PagesLoader >
           <div className="homePage flex flex-col items-center justify-center min-h-screen">
       <p
         className="text-center uppercase text-[40px] md:text-[80px] font-bold pl-3 animate-rotateIn"
@@ -43,6 +43,6 @@ export default function Home() {
         view all characters
       </Link>
     </div>
-    </PagesLoader>
+    // </PagesLoader>
   );
 }
