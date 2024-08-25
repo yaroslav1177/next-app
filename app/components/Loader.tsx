a23c6c0c9a9e4d58f2bd91498516e970ffb2f006
Yaroslav Avramidi <passenger1177@gmail.com>
2024-08-26 00:30:04 +0300

    improve main loader

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -15,8 +15,10 @@ const LottieLoader = () => {
   return (
     <div className="lottie-container">
       <div className='flex flex-col items-center gap-6 animate__animated animate__zoomIn'>
-      <Lottie options={defaultOptions} height={400} width={400} />
-      <p className='text-2xl md:text-8xl'>wait a second...</p>
+        <Lottie options={defaultOptions} height={400} width={400} />
+        <p className='text-2xl md:text-8xl flex items-center'>
+          wait a second <span className="dots"></span>
+        </p>
       </div>
     </div>
   );
